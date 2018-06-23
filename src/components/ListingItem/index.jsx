ffd9bc455288369a33ebe59ab267ac5b3a6ec716
miketheo423 <miketheo423@gmail.com>
2018-06-23 16:04:49 -0600

    Adds font awesome and icon components and wires them into listing item

--- src/components/ListingItem/index.jsx
+++ src/components/ListingItem/index.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.scss';
 
+import Icon from '../common/Icon';
+
 class ListingItem extends Component {
   constructor(props) {
     super(props);
@@ -15,6 +17,10 @@ class ListingItem extends Component {
     const { title, url } = this.state;
     return (
       <article className={styles.listingItem}>
+        <div className={styles.iconContainer}>
+          <Icon iconClass="fas fa-pen" />
+          <Icon iconClass="fa fa-trash" />
+        </div>
         <h2>{title}</h2> 
         <h3>{url}</h3>
       </article>
