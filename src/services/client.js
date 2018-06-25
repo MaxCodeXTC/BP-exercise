c32d7ee2b4e8450fb3797fd0ecdb62d5d9c3f384
miketheo423 <miketheo423@gmail.com>
2018-06-24 21:36:15 -0600

    Putting token in correct env file

--- src/services/client.js
+++ src/services/client.js
@@ -14,6 +14,7 @@ const API = axios.create({
 export default {
   getListings() {
     return API.get('/listings').then(response => {
+      console.log(response);
       return serializer(response.data);
     });
   },
